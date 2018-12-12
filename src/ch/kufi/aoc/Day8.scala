89b6fcf588dfb7bc96940134e938951c05c7def7
Patrick Kaufmann <patrick.kaufmann@innoq.com>
2018-12-12 11:52:49 +0100

    Day 8: Added recursive solution

--- src/ch/kufi/aoc/Day8.scala
+++ src/ch/kufi/aoc/Day8.scala
@@ -1,9 +1,11 @@
 package ch.kufi.aoc
 
-class Day8 extends Challenge[Int, List[List[Int]]] {
+class Day8 extends Challenge[(Int, Int), (Int, List[List[Int]])] {
 
-  override def part1(): Int = {
-    Stream
+  override def part1(): (Int, Int) = {
+    val tree = createTree(readEntries())._2
+
+    val iterativeSum = Stream
       .iterate((0, List[State](Subnode), readEntries())) {
         case (checksum, Subnode :: remainingStates, childNodeCount :: metadataCount :: unparsed) =>
           (checksum, List.fill(childNodeCount)(Subnode) ::: Metadata(metadataCount) :: remainingStates, unparsed)
@@ -14,10 +16,55 @@ class Day8 extends Challenge[Int, List[List[Int]]] {
       .find(_._2.isEmpty)
       .head
       ._1
+
+    (tree.sumMetadata, iterativeSum)
   }
 
-  override def part2(): List[List[Int]] = {
-    Stream
+  def createTree(entries: List[Int]): (List[Int], Node) = {
+    val (entry, unparsed) = entries.splitAt(2)
+    val subnodeCount = entry.head
+    val metadataCount = entry.last
+
+    if(subnodeCount == 0) {
+      val (metadata, restUnparsed) = unparsed.splitAt(metadataCount)
+      (restUnparsed, Node(List.empty, metadata))
+    } else {
+      val (restUnparsed, subnodes) = Stream
+        .iterate((unparsed, List.empty[Node])) {
+          case (unparsed, subnodes) =>
+            val (u, n) = createTree(unparsed)
+            (u, subnodes :+ n)
+        }
+        .drop(subnodeCount)
+        .head
+
+      val (metadata, restU) = restUnparsed.splitAt(metadataCount)
+
+      (restU, Node(subnodes, metadata))
+    }
+  }
+
+  case class Node(subnodes: List[Node], metadata: List[Int]) {
+    def sumMetadata: Int = {
+      subnodes.foldLeft(0)(_ + _.sumMetadata) + metadata.sum
+    }
+
+    def nodeValue: Int = {
+      if(subnodes.isEmpty) {
+        metadata.sum
+      } else {
+        metadata.foldLeft(0) {
+          case (sum, index) =>
+            sum + subnodes.lift(index - 1).map(_.nodeValue).getOrElse(0)
+        }
+      }
+    }
+  }
+
+  override def part2(): (Int, List[List[Int]]) = {
+    val tree = createTree(readEntries())._2
+
+    val iterativeSum = Stream
       .iterate((List(List.empty[Int]), List[State](Subnode), readEntries())) {
         case (subNodes :: otherNodes, Subnode :: nextStates, 0 :: metadataCount :: unparsed) =>
           val (metadata, remainingUnparsed) = unparsed.splitAt(metadataCount)
@@ -37,6 +84,8 @@ class Day8 extends Challenge[Int, List[List[Int]]] {
       .find(_._2.isEmpty)
       .head
       ._1
+
+    (tree.nodeValue, iterativeSum)
   }
 
   private def readEntries() = {
